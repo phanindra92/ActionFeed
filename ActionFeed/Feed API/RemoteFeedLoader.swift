934a4fbd19d8f21991e138600f94f64fa5a88c1b
phanindra92 <phanindra.kb92@gmail.com>
2021-12-21 16:11:31 +0530

    Move remote feed loader components into productions

---
+++ ActionFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  ActionFeed
+//
+//  Created by phanindra on 21/12/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    let url: URL
+    let client: HTTPClient
+    
+    public init (url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
