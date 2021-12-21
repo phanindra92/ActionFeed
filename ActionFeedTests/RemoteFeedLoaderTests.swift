53645682b76976c0bc37fa3ef7f65cdef766836a
phanindra92 <phanindra.kb92@gmail.com>
2021-12-21 16:47:53 +0530

    Removing subbing from spy

--- ActionFeedTests/RemoteFeedLoaderTests.swift
+++ ActionFeedTests/RemoteFeedLoaderTests.swift
@@ -37,10 +37,12 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0, userInfo: nil)
         
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0, userInfo: nil)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivityError])
     }
@@ -57,13 +59,15 @@ class RemoteFeedLoaderTests: XCTestCase {
 
 class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
-    var error: Error?
+    var completions = [(Error) -> Void]()
     
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-        if let error = error {
-            completion(error)
-        }
+        completions.append(completion)
         requestedURLs.append(url)
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        completions[index](error)
     }
     
 }
