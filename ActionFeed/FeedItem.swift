068dbb46284eed12bbf4d7941ff1c280f9de7b73
phanindra92 <phanindra.kb92@gmail.com>
2021-12-21 13:26:37 +0530

    Create Feed item model in framework

---
+++ ActionFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  ActionFeed
+//
+//  Created by phanindra on 21/12/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
