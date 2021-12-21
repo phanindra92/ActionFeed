f65b5e65b23146f87a1c5dbbb57a633b2b0ff3f7
phanindra92 <phanindra.kb92@gmail.com>
2021-12-21 15:22:39 +0530

    Making http client as global mutable state to create spy

--- ActionFeedTests/RemoteFeedLoaderTests.swift
+++ ActionFeedTests/RemoteFeedLoaderTests.swift
@@ -7,40 +7,45 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
+}
+
+class HTTPClient {
+    static var shared = HTTPClient()
+    func get(from url: URL) {}
+}
+
 class RemoteFeedLoaderTests: XCTestCase {
 
-    class RemoteFeedLoader {
-        let client: HTTPClient
-        
-        init(client: HTTPClient) {
-            self.client = client
-        }
-        
-        func load() {
-            client.requestedURL = URL(string: "https://a-url.com")
-        }
-    }
-    
-    class HTTPClient {
-        var requestedURL: URL?
-    }
-    
-
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader(client: client)
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader()
         
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient()
-        let sut = RemoteFeedLoader(client: client)
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader()
+        HTTPClient.shared = client
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
     }
 
+}
+
+//MARK: Helpers
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
+    
+    var requestedURL: URL?
 }
