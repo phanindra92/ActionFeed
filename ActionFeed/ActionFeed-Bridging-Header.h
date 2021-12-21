226a5ce9af5a1477bf5a0e70a868666915648ce0
phanindra92 <phanindra.kb92@gmail.com>
2021-12-21 14:54:49 +0530

    Adding bridging header

---
+++ ActionFeed/ActionFeed-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ActionFeed-Bridging-Header.h
+//  ActionFeed
+//
+//  Created by phanindra on 21/12/21.
+//
+
+#ifndef ActionFeed_Bridging_Header_h
+#define ActionFeed_Bridging_Header_h
+
+
+#endif /* ActionFeed_Bridging_Header_h */
---
+++ ActionFeedTests/ActionFeedTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ActionFeedTests-Bridging-Header.h
+//  ActionFeed
+//
+//  Created by phanindra on 21/12/21.
+//
+
+#ifndef ActionFeedTests_Bridging_Header_h
+#define ActionFeedTests_Bridging_Header_h
+
+
+#endif /* ActionFeedTests_Bridging_Header_h */
