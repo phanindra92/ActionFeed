62087f54951cf7668a3442b792ae92f66ca11ac2
phanindra92 <phanindra.kb92@gmail.com>
2022-01-02 19:44:54 +0530

    Retrieve from invalid cache twice delivers dame failure twice (has no-side effect)

--- ActionFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ ActionFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -119,6 +119,16 @@ class CodableFeedStoreTests: XCTestCase {
         
         expect(sut, toRetrieve: .failure(anyNSError()))
     }
+    
+    func test_retrieve_hasNoSideEffectOnFailure() {
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
+
+        try! "invalid data".write(to: storeURL, atomically: true, encoding: .utf8)
+        
+        expect(sut, toRetrieveTwice: .failure(anyNSError()))
+    }
+
 
     //MARK: - Helpers
     
