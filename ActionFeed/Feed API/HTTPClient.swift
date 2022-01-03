7273da934a618193843608ab7dfb31f87e0a716c
phanindra92 <phanindra.kb92@gmail.com>
2022-01-03 14:49:16 +0530

    Add comment to document 'HTTPClient' implementation can be invoked in any thread. Clients also responsible to dispatch to appropriate threads, if needed.

--- ActionFeed/Feed API/HTTPClient.swift
+++ ActionFeed/Feed API/HTTPClient.swift
@@ -13,5 +13,7 @@ public enum HTTPClientResult {
 }
 
 public protocol HTTPClient {
+    /// Completion handler can be invoked in any thread.
+    /// Clients also responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
