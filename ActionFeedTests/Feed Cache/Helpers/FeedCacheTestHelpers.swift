239d4c239d22c7bbf35fd572722a20936b75390b
phanindra92 <phanindra.kb92@gmail.com>
2022-01-12 21:18:24 +0530

    Moving the unique image and unique image feed methods to respective file

--- ActionFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ ActionFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -8,6 +8,16 @@
 import Foundation
 import ActionFeed
 
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (models, local)
+}
+
+func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+}
+
 extension Date {
     private var feedCacheMaxAgeInDays: Int { return 7 }
     
